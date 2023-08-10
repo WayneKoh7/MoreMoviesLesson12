00e31059eb8cd0cb73d134d8340e1f2add9c48e8
Wayne <22000210@myrp.edu.sg>
2023-08-10 23:14:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22000210/moremovieslesson12/Edit.java
@@ -0,0 +1,110 @@
+package sg.edu.rp.c346.id22000210.moremovieslesson12;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Edit extends AppCompatActivity {
+
+    EditText etId, etTitle, etGenre, etYear;
+    Spinner spinner;
+    Button btnUpdate, btnDelete, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        etId = findViewById(R.id.etId);
+        etTitle = findViewById(R.id.etMovieTitle);
+        etGenre = findViewById(R.id.etGenre);
+        etYear = findViewById(R.id.etYear);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnCancel = findViewById(R.id.btnCancel);
+        spinner = findViewById(R.id.rating);
+
+        Intent intent = getIntent();
+        Movie data = (Movie) intent.getSerializableExtra("movie");
+        etId.setText(String.valueOf(data.getId()));
+        etTitle.setText(data.getTitle());
+        etGenre.setText(data.getGenre());
+        etYear.setText(String.valueOf(data.getYear()));
+        spinner.setSelection(getIndex(spinner, data.getRating()));
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper db = new DBHelper(Edit.this);
+                String selectedItem = spinner.getSelectedItem().toString();
+                data.setTitle(etTitle.getText().toString());
+                data.setGenre(etGenre.getText().toString());
+                data.setYear(Integer.parseInt(etYear.getText().toString()));
+                data.setRating(selectedItem);
+                db.updateMovie(data);
+                db.close();
+                finish();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myBuilder=new AlertDialog.Builder(Edit.this);
+
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to delete the movie "+data.getTitle());
+                myBuilder.setPositiveButton("CANCEL", null);
+
+                myBuilder.setNegativeButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DBHelper db = new DBHelper(Edit.this);
+                        db.deleteMovie(data.getId());
+                        finish();
+                    }
+                });
+
+                AlertDialog myDialog=myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myBuilder=new AlertDialog.Builder(Edit.this);
+
+                myBuilder.setTitle("Danger");
+                myBuilder.setMessage("Are you sure you want to discard the changes");
+                myBuilder.setPositiveButton("DO NOT DISCARD", null);
+
+                myBuilder.setNegativeButton("DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+                AlertDialog myDialog=myBuilder.create();
+                myDialog.show();
+            }
+        });
+
+    }
+    private int getIndex(Spinner spinner, String myString){
+        for (int i = 0; i < spinner.getCount(); i++){
+            if (spinner.getItemAtPosition(i).toString().equalsIgnoreCase(myString)){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
